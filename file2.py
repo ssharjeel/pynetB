e58e99f309dda3ec5922ea5ea9e05f37e66612ba
Syed Adnan Sharjeel <sharjeel.cisco@gmail.com>
2025-10-16 14:08:23 -0700

    Modifying file2.py

--- file2.py
+++ file2.py
@@ -3,10 +3,10 @@
 # Take input from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
+num3 = float(input("Enter Third number: "))
 # Add the numbers
-sum_result = num1 + num2
+sum_result = num1 + num2 + num3
 
 # Display the result
-print(f"The sum of {num1} and {num2} is {sum_result}")
+print(f"The sum of {num1},{num2} and {num3} is {sum_result}")
 
